e790ed860d22388a478d704e17a77cbec43af285
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-15 21:05:54 +0300

    add for 100

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿for (int i = 0; i < 100; i++) {
 
-Console.WriteLine("Hello, World!");
+    Console.WriteLine($"Hello, World! N {i}");
+}
+
