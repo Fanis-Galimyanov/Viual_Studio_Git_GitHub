211fab06d048dbdbe95901ce1bec0addb3c605cc
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-15 21:30:05 +0300

    ITPark Hello

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿
-/*var a = new Object();
+var a = new Object();
 var b = new Object();
 var c = new Object();
 
+
+Console.Write("ITPark, Hello!!!");
 class Same
 {
     string same { get; set; }
 }
-*/
